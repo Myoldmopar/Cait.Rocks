07951a7124058a0cb1d64482d679356442434b44
Edwin Lee <leeed2001@gmail.com>
2018-04-22 13:04:51 -0500

    Add recipe type; cleanup front end

--- recipes/models.py
+++ recipes/models.py
@@ -4,8 +4,36 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class RecipeTypes(object):
+    UNKNOWN = '10'
+    WHOLE_MEAL = '20'
+    ENTREE = '30'
+    SOUP = '40'
+    SALAD = '50'
+    DRINK = '60'
+    DESSERT = '70'
+    SIDE_DISH = '80'
+    SAUCE_DRESSING = '90'
+    SEASONING = '100'
+
+
 class Recipe(models.Model):
+
     title = models.CharField(max_length=100)
+
+    RECIPE_TYPE_CHOICES = (
+        (RecipeTypes.UNKNOWN, "Unknown"),
+        (RecipeTypes.WHOLE_MEAL, "Entire Meal"),
+        (RecipeTypes.ENTREE, "Entree"),
+        (RecipeTypes.SOUP, "Soup"),
+        (RecipeTypes.SALAD, "Salad"),
+        (RecipeTypes.DRINK, "Drink"),
+        (RecipeTypes.DESSERT, "Dessert"),
+        (RecipeTypes.SIDE_DISH, "Side Dish"),
+        (RecipeTypes.SAUCE_DRESSING, "Sauce/Dressing"),
+        (RecipeTypes.SEASONING, "Seasoning"),
+    )
+    recipe_type = models.CharField(max_length=20, choices=RECIPE_TYPE_CHOICES, default='Unknown')
 
     def __str__(self):
         return self.title
--- reciplees/urls.py
+++ reciplees/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
+from django.http import HttpResponseRedirect
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # ex: /recipes
     url(r'^recipes/', include('recipes.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('recipes/')),
 ]

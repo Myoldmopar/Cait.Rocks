ce4c71e68029233166aadba9b749860b65ca8b65
Edwin Lee <leeed2001@gmail.com>
2018-04-22 09:20:41 -0500

    Add bootstrap and style

--- reciplees/settings.py
+++ reciplees/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dpvt@xbg@^81u%a^2nd-$-x6lzlpcs(jtzscp@#2!+4!+plwby'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.10']
 
 
 # Application definition
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "bower_components")
+]

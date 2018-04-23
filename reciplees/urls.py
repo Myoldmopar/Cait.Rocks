3d198ab89ca81d81374815dfb2e896bcdd450555
Edwin Lee <leeed2001@gmail.com>
2018-04-22 21:20:58 -0500

    Add issue reporting link

--- reciplees/urls.py
+++ reciplees/urls.py
@@ -3,8 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    # ex: /recipes
+    url(r'^admin/', admin.site.urls, name="admin"),
     url(r'^recipes/', include('recipes.urls')),
     url(r'^$', lambda r: HttpResponseRedirect('recipes/')),
 ]

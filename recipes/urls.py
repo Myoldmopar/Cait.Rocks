16e983a4dc9fc65506e691abe4d4030e00180c43
Edwin Lee <leeed2001@gmail.com>
2018-04-26 08:29:32 -0500

    Clean up some more urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,14 +1,19 @@
 from django.conf.urls import url
 
-from . import views
+from recipes.views import get_monthly_view, grocery_list, monthly_plan, cookbook, detail_recipe
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
 
     # Main cookbook entry page
-    url(r'^$', views.cookbook, name='cookbook'),
+    url(r'^$', cookbook, name='cookbook'),
 
     # Detailed recipe entry page
-    url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.detail_recipe, name='detail'),
+    url(r'^recipe/(?P<recipe_id>[0-9]+)/$', detail_recipe, name='detail'),
+
+    # Monthly Plan pages, ultimately containing a nice calendar and table of ingredients for grocery shopping
+    url(r'^thismonth/', get_monthly_view),
+    url(r'^grocery_list/', grocery_list),
+    url(r'^monthly_plan/', monthly_plan),
 
 ]
--- reciplees/urls.py
+++ reciplees/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 from rest_framework_swagger.views import get_swagger_view
 
-from recipes.views import get_monthly_view, grocery_list, monthly_plan
-
 schema_view = get_swagger_view(title='RecipLees API')
 
 handler404 = 'recipes.views.handle404'
@@ -19,11 +17,6 @@ urlpatterns = [
 
     # Swagger API documentation page
     url(r'^swagger/', schema_view),
-
-    # Monthly Plan page, ultimately containing a nice calendar and table of ingredients for grocery shopping
-    url(r'^thismonth/', get_monthly_view),
-    url(r'^grocery_list/', grocery_list),
-    url(r'^monthly_plan/', monthly_plan),
 
     # Main page, listing all the recipes
     url(r'^cookbook/', include('recipes.urls')),

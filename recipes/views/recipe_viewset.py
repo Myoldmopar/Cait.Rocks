4f5e154f6326c942befbab852ea9e0c2df5a5bd6
Edwin Lee <leeed2001@gmail.com>
2018-04-27 06:24:47 -0500

    Measurements showing up, but at the cost of a kluge in the retrieve method

--- recipes/views/recipe_viewset.py
+++ recipes/views/recipe_viewset.py
@@ -11,4 +11,13 @@ class RecipeViewSet(ViewSet):
 
     def retrieve(self, request, pk=None):
         recipe = get_object_or_404(Recipe, pk=pk)
-        return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+        ingredient_strings = []
+        for ingredient in recipe.ingredient_set.all():
+            this_ingedient_string = ''
+            if ingredient.amount:
+                this_ingedient_string += ingredient.get_amount_display() + ' '
+            if ingredient.measurement:
+                this_ingedient_string += ingredient.get_measurement_display() + ' '
+            this_ingedient_string += ingredient.item_description
+            ingredient_strings.append(this_ingedient_string)
+        return render(request, 'recipes/recipe_detail.html', {'recipe': recipe, 'ingredients': ingredient_strings})

5ddacf412303c3ddcaafbceec521fe71a2896786
Edwin Lee <leeed2001@gmail.com>
2018-05-09 19:53:51 -0500

    Refining some tests, back to 100% passing

--- recipes/tests/models/test_ingredient.py
+++ recipes/tests/models/test_ingredient.py
@@ -10,19 +10,19 @@ class IngredientModelConstructionTests(TestCase):
         Makes sure ingredient can potentially be blank...do we want this?
         """
         blank_ingredient = Ingredient(item_description='')
-        self.assertIs(str(blank_ingredient), '')
+        self.assertIs(unicode(blank_ingredient), u'')
 
     def test_ingredient_default_construction(self):
         """
         Makes sure ingredient can potentially be blank...do we want this?
         """
         blank_ingredient = Ingredient()
-        self.assertIs(str(blank_ingredient), '')
+        self.assertIs(unicode(blank_ingredient), u'')
 
     def test_ingredient_normal_construction(self):
-        ingredient_string = 'This is a normal ingredient'
-        blank_ingredient = Ingredient(item_description=ingredient_string)
-        self.assertIs(str(blank_ingredient), ingredient_string)
+        ingredient_string = u'This is a normal ingredient'
+        proper_ingredient = Ingredient(item_description=ingredient_string)
+        self.assertIs(unicode(proper_ingredient), ingredient_string)
 
 
 class IngredientFullStringFunctionTests(TestCase):
@@ -34,7 +34,7 @@ class IngredientFullStringFunctionTests(TestCase):
         self.assertEqual(i.measurement, expected_measurement)
         expected_description = kwargs.get('item_description', '')
         self.assertEqual(i.item_description, expected_description)
-        self.assertEqual(i.full_string(), expected_full_string)
+        self.assertEqual(unicode(i), expected_full_string)
 
     def test_full_string_combinations(self):
         """

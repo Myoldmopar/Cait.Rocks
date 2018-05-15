8a884a86b73e82d6fe4d908bf1e04f7c060abba0
Edwin Lee <leeed2001@gmail.com>
2018-05-14 23:41:19 -0500

    Fix karma tests

--- scripts/run_npm_tests.sh
+++ scripts/run_npm_tests.sh
@@ -3,13 +3,13 @@
 if [ -z ${TRAVIS+x} ]; then
     # Travis is not set, npm install should already have been done on this development machine, just run tests
     export DISPLAY=:0
-    npm test -- --browsers Chrome,Firefox  # I'm assuming we have Chrome and Firefox locally, test both!
+    yarn test -- --browsers Chrome,Firefox  # I'm assuming we have Chrome and Firefox locally, test both!
     exit $?
 else
     # Travis is set, need to set up the headless stuff, make sure we install npm dependencies, and then run tests
-    npm install
+    yarn install
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
-    npm test -- --browsers Firefox  # Travis doesn't have Chrome
+    yarn test -- --browsers Firefox  # Travis doesn't have Chrome
     exit $?
 fi

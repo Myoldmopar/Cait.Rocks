d35c4f344aba95a5beb1f0883f3de7aeb4bc8059
Edwin Lee <leeed2001@gmail.com>
2018-05-15 14:45:07 -0500

    Scratch that last commit; we are just using Firefox

--- scripts/run_npm_tests.sh
+++ scripts/run_npm_tests.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# Chrome was really slow and always had the same results as Firefox, and Travis doesn't have Chrome by default,
+# so we're just going to always use only Firefox
+
 if [ -z ${TRAVIS+x} ]; then
     # Travis is not set, yarn install should already have been done on this development machine, just run tests
     export DISPLAY=:0
-    yarn test -- --browsers Chrome  # Firefox was really slow and always had the same results as Chrome
+    yarn test -- --browsers Firefox
     exit $?
 else
     # Travis is set, need to set up the headless stuff, make sure we install npm dependencies, and then run tests
     yarn install
     export DISPLAY=:99.0
     sh -e /etc/init.d/xvfb start
-    yarn test -- --browsers Firefox  # Travis doesn't have Chrome
+    yarn test -- --browsers Firefox
     exit $?
 fi

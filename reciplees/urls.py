aca5ec670706612e36d70d1d477cea8f018f42b3
Edwin Lee <leeed2001@gmail.com>
2018-05-15 19:22:14 -0500

    add humans and robots files, with tests; fixes #52

--- reciplees/urls.py
+++ reciplees/urls.py
@@ -20,6 +20,12 @@ urlpatterns = [
     # "About" page with Git sha and version info
     url(r'^about/', server_version_data, name='about'),
 
+    # ... your project urls
+    url(r'^robots.txt$', TemplateView.as_view(template_name="common/robots.txt", content_type="text/plain"),
+        name="robots_file"),
+    url(r'^humans.txt$', TemplateView.as_view(template_name="common/humans.txt", content_type="text/plain"),
+        name="humans_file"),
+
     # Root home page
     url(r'^$', TemplateView.as_view(template_name='common/home.html', content_type="text/html"), name="home")
 

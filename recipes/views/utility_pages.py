dec33b6600d1b306e5c4d5a962c06d154f6f5acc
Edwin Lee <leeed2001@gmail.com>
2018-05-16 21:14:32 -0500

    Add informative git message

--- recipes/views/utility_pages.py
+++ recipes/views/utility_pages.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from django.shortcuts import render
 from rest_framework import status
 from subprocess import check_output, CalledProcessError
@@ -13,14 +15,20 @@ def server_version_data(request):
     # get the actual server version
     settings_version = settings.VERSION
 
+    if os.path.exists('/usr/bin/git'):
+        git_message = '(/usr/bin/git is present!)'
+    else:
+        git_message = '(/usr/bin/git is not found!)'
+
     # get the current Git sha
     try:
         git_sha = check_output(['git', 'rev-parse', 'HEAD']).strip()
         # git_sha = git_sha[0:10]
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not get current git sha, does this server not have Git?>'
+
+        git_sha = '<Could not get current git sha> ' + git_message
     except OSError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not get current git sha, does this server not have Git?>'
+        git_sha = '<Could not get current git sha>' + git_message
 
     # get a flag for whether there are local uncommitted changes
     working_dir_clean_message = 'Clean'
@@ -29,9 +37,9 @@ def server_version_data(request):
         if local_diff_status != u'':  # pragma: no cover
             working_dir_clean_message = 'Local uncommitted changes!'
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...
-        working_dir_clean_message = '<Could not check working directory cleanliness, does this server not have Git?>'
+        working_dir_clean_message = '<Could not check working directory cleanliness>' + git_message
     except OSError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not check working directory cleanliness, does this server not have Git?>'
+        git_sha = '<Could not check working directory cleanliness>' + git_message
 
     return render(
         request,

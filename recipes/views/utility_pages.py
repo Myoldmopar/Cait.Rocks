070e7032b744c6527ed82f10c5f0e76020eec60f
Edwin Lee <leeed2001@gmail.com>
2018-05-16 21:22:53 -0500

    Use Heroku environ variable for source version!

--- recipes/views/utility_pages.py
+++ recipes/views/utility_pages.py
@@ -15,24 +15,17 @@ def server_version_data(request):
     # get the actual server version
     settings_version = settings.VERSION
 
-    if os.path.exists('.git'):
-        git_repo = ' (This is a repo)'
-    else:
-        git_repo = ' (This is NOT a repo)'
-
-    if os.path.exists('/usr/bin/git'):
-        git_message = ' (/usr/bin/git is present!)'
-    else:
-        git_message = ' (/usr/bin/git is not found!)'
-
     # get the current Git sha
     try:
         git_sha = check_output(['git', 'rev-parse', 'HEAD']).strip()
         # git_sha = git_sha[0:10]
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not get current git sha>' + git_message + git_repo
+        if 'SOURCE_VERSION' in os.environ:
+            git_sha = os.environ['SOURCE_VERSION']
+        else:
+            git_sha = '<Could not get current git sha>'
     except OSError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not get current git sha>' + git_message + git_repo
+        git_sha = '<Could not get current git sha>'
 
     # get a flag for whether there are local uncommitted changes
     working_dir_clean_message = 'Clean'
@@ -41,9 +34,9 @@ def server_version_data(request):
         if local_diff_status != u'':  # pragma: no cover
             working_dir_clean_message = 'Local uncommitted changes!'
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...
-        working_dir_clean_message = '<Could not check working directory cleanliness>' + git_message + git_repo
+        working_dir_clean_message = 'Could not check working directory; we\'re probably deployed on a server, so clean'
     except OSError:  # pragma: no cover -- This would be crazy to try to test...
-        git_sha = '<Could not check working directory cleanliness>' + git_message + git_repo
+        working_dir_clean_message = 'Could not check working directory; we\'re probably deployed on a server, so clean'
 
     return render(
         request,

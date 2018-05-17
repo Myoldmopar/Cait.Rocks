1d7b1556485805fb3d64c8c95bb0555231ae42d2
Edwin Lee <leeed2001@gmail.com>
2018-05-16 21:09:22 -0500

    Remove git static binary; didnt work

--- recipes/views/utility_pages.py
+++ recipes/views/utility_pages.py
@@ -15,7 +15,7 @@ def server_version_data(request):
 
     # get the current Git sha
     try:
-        git_sha = check_output(['bin/git', 'rev-parse', 'HEAD']).strip()
+        git_sha = check_output(['git', 'rev-parse', 'HEAD']).strip()
         # git_sha = git_sha[0:10]
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...
         git_sha = '<Could not get current git sha, does this server not have Git?>'
@@ -25,7 +25,7 @@ def server_version_data(request):
     # get a flag for whether there are local uncommitted changes
     working_dir_clean_message = 'Clean'
     try:
-        local_diff_status = check_output(['bin/git', 'status', '--porcelain'])
+        local_diff_status = check_output(['git', 'status', '--porcelain'])
         if local_diff_status != u'':  # pragma: no cover
             working_dir_clean_message = 'Local uncommitted changes!'
     except CalledProcessError:  # pragma: no cover -- This would be crazy to try to test...

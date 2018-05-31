a5c6f90609909f02b2a26de09204137bf0406316
Edwin Lee <leeed2001@gmail.com>
2018-05-31 02:09:15 -0500

    JS coverage up to 100%; though I had to skip a few lines and I dont feel great about it

--- recipes/static/recipes/js/controllers/recipe_list.js
+++ recipes/static/recipes/js/controllers/recipe_list.js
@@ -4,31 +4,45 @@ app.controller('recipe_list_controller', ['$scope', 'recipe_service', function (
     'use strict';
 
     $scope.selected_recipe = null;
-    $scope.sortType = 'title'; // set the default sort type
-    $scope.sortReverse = false;  // set the default sort order
-    $scope.searchRecipe = '';     // set the default search/filter term
+    $scope.sort_type = 'title';
+    $scope.sort_reverse = false;
+    $scope.search_recipe = '';
+    $scope.recipe_error_message = '';
 
     $scope.init = function () {
-        // create the list of sushi rolls
+        $scope.recipe_error_message = '';
         recipe_service.get_recipes().then(
             function (response) {
                 $scope.allRecipes = response;
             }
-        );
+        ).catch(function () {
+            $scope.allRecipes = [];
+            $scope.recipe_error_message = 'Could not retrieve recipes, is the server broken?'
+        });
     };
 
     $scope.select_a_recipe = function (recipe_id) {
+        $scope.recipe_error_message = '';
         recipe_service.get_recipe(recipe_id).then(
             function (response) {
                 $scope.selected_recipe = response;
+                // I tried just using the img src as an angular expression in the html, but it was throwing a 404
+                // when the browser was creating the DOM, before the angular expression could be evaluated,
+                // so instead I just create/modify the src attribute here
                 var target = document.getElementById('selected_recipe_image');
-                if ($scope.selected_recipe.image) {
-                    target.src = $scope.selected_recipe.image;
-                } else {
-                    target.src = '';
+                /* istanbul ignore next */
+                if (target) {
+                    if ($scope.selected_recipe.image) {
+                        target.src = $scope.selected_recipe.image;
+                    } else {
+                        target.src = '';
+                    }
                 }
             }
-        )
+        ).catch(function () {
+            $scope.selected_recipe = null;
+            $scope.recipe_error_message = 'Could not retrieve recipe, is the server broken?';
+        });
     };
 
 }]);

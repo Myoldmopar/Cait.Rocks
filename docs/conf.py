c25a00e1fbe475bb39fd97807eb38359be08b7e5
Edwin Lee <leeed2001@gmail.com>
2018-07-07 13:06:18 -0500

    Conditionally depend on sphinx spelling only on Travis

--- docs/conf.py
+++ docs/conf.py
@@ -56,8 +56,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinxcontrib.spelling',
 ]
+
+if os.environ['TRAVIS']:
+    extensions.append('sphinxcontrib.spelling')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
